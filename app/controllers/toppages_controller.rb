d9654303622f17437e13d125d2353007da3bdde0
kenji.komachi <rgby1308xazkk@gmail.com>
2020-04-23 13:17:24 +0000

    user-index

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -6,11 +6,10 @@ class ToppagesController < ApplicationController
     end
     
     # ARGV[0]で実行時の1つめのパラメータを取得、存在しない場合は'Ruby'を設定
-    keyword = ARGV[0] || 'basketball'
+    keyword = ARGV[0] || 'ball'
     
     # rakuten_web_serviceの使用法に乗っ取りHTTPリクエストを送ってデータを取得
     @items = RakutenWebService::Ichiba::Item.search(keyword: keyword)
-    
     # 取得したデータを10件まで表示
     #items.first(10).each do |item|
       #puts "#{item['itemName']}, #{item.price} yen"
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(25)
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
