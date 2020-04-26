74283c97eb521ae15e1fd547455be043bd5a00f1
kenji.komachi <rgby1308xazkk@gmail.com>
2020-04-26 11:07:03 +0000

    image upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:image, :name, :email, :password, :password_confirmation)
   end
   
 end
