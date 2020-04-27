c34011b43a9bae092b219362c7ca1b17d520b522
kenji.komachi <rgby1308xazkk@gmail.com>
2020-04-27 12:51:49 +0000

    uniqness-itemcode

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 255 }
+    validates :price, presence: true, length: { maximum: 50 }
+    validates :content, presence: true, length: { maximum: 255 }
+    validates :item_code, presence: true, length: { maximum: 50 }, uniqueness: true
 end
